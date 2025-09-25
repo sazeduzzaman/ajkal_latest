6c921417be975dd918ca3bd18ad5862526a14ee9
Szamansaju <szamansaju@gmail.com>
2025-09-25 14:57:07 +0600

    Error and loading page updated

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+"use client";
+import React from "react";
+import { HashLoader } from "react-spinners";
 
-const loading = () => {
-    return (
-        <div>
-            Loading...
-        </div>
-    );
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      <HashLoader color="#165588" size={60} />
+    </div>
+  );
 };
 
-export default loading;
+export default Loading;
